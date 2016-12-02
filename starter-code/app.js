ac97a12d0a805aeafa109aec61194ad1238de8ef
merryschurr <merryschurr@gmail.com>
2016-12-01 23:21:08 -0700

    adds js

--- starter-code/app.js
+++ starter-code/app.js
@@ -4,14 +4,24 @@
 var express = require('express');
 var app = express();
 var bodyParser = require('body-parser');
-
-// How do we 'require' the candyRouter file?
-var candyRouter;
+var candyRouter = require ('./candyRouter.js');
+console.log (candyRouter);
 
 app.use(bodyParser.json());
+
+
+
+
 
 //How do we redirect the /candies path
 //through our candyRouter?
 //Hint: you need app.use
+app.use('/candies', candyRouter);
+
+//res.json(candy);
+
+
 
 app.listen(3000);
+
+// a "GET" request to "/" will run the function below
--- starter-code/candyRouter.js
+++ starter-code/candyRouter.js
@@ -2,16 +2,50 @@ var express = require('express');
 var bodyParser = require('body-parser');
 router = express.Router();
 
-var candies;
+var candy = [{"id":1,"name":"Chewing Gum","color":"Red"},{"id":2,"name":"Pez","color":"Green"},{"id":3,"name":"Marshmallow","color":"Pink"},{"id":4,"name":"Candy Stick","color":"Blue"}];
 
 //What would need to go into candies
 //in order to pass our first test?
 
+//GET/SHOW all candies in array
 router.get('/', function(req,res) {
-	// What would go here? 
-	// Hint: we want all candies in JSON format
+	res.json(candy);
 });
 
-// Fill out the rest of the routes here
+//GET/SHOW candy id 3 at 3000/candy/3
+router.get('/:id', function(req, res) {
+	res.send({"id":3,"name":"Marshmallow","color":"Pink"});
+});
+
+//POST/CREATE 5th candy
+router.post('/', function(req, res) {
+	res.send({"id":5,"name":"Jelly Belly","color":"Orange"});
+}); 
+
+//GET/SHOW all candies
+router.get('/', function(req,res) {
+	res.json(candy);
+});
+
+//PUT/UPDATE candy id 3 color
+router.put('/:id', function(req, res) {
+	res.send({"name":"Marshmallows","color":"white"});
+});
+
+//GET/SHOW all candies
+router.get('/', function(req,res) {
+	res.json(candy);
+});
+
+//DELETE candy id 2
+router.delete ('/:id', function(req, res) {
+	res.json({"id":2,"name":"Pez","color":"Green"});
+});
+
+//GET/SHOW all candies
+router.get('/', function(req,res) {
+	res.json(candy);
+});
 
 module.exports = router;
+
